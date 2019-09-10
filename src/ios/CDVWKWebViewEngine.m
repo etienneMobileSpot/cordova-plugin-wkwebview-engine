c31fe193f89182245afcd8dfb301015e755b0282
Etienne Lescot <etienne.lescot@mobile-spot.com>
2019-09-10 10:51:04 +0200

    +++

--- src/ios/CDVWKWebViewEngine.m
+++ src/ios/CDVWKWebViewEngine.m
@@ -126,6 +126,7 @@ API_AVAILABLE(ios(12.0)){
 
 - (WKWebViewConfiguration*) createConfigurationFromSettings:(NSDictionary*)settings
 {
+
     WKWebViewConfiguration* configuration = [[WKWebViewConfiguration alloc] init];
     configuration.processPool = [[CDVWKProcessPoolFactory sharedFactory] sharedProcessPool];
     if (settings == nil) {
@@ -484,6 +485,9 @@ static void * KVOContext = &KVOContext;
 
 - (void) webView: (WKWebView *) webView decidePolicyForNavigationAction: (WKNavigationAction*) navigationAction decisionHandler: (void (^)(WKNavigationActionPolicy)) decisionHandler
 {
+    if (! @available(iOS 12.0, *)) {
+        return;
+    }
     NSURL* url = [navigationAction.request URL];
     CDVViewController* vc = (CDVViewController*)self.viewController;
 
